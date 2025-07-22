e22b4028b84b29a9424331ee9eef0a3d1020a5c6
tommy-jin <tommy@bumpsocial.app>
2025-07-22 17:05:23 +0900

    remove custom DispatcherServletRegistrationBean and related configuration for page handling in DemoApplication; simplify application startup

--- src/main/kotlin/com/meeting/demo/DemoApplication.kt
+++ src/main/kotlin/com/meeting/demo/DemoApplication.kt
@@ -1,32 +1,10 @@
 package com.meeting.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
-import org.springframework.web.servlet.DispatcherServlet
 
 @SpringBootApplication
-class DemoApplication {
-    
-    @Bean
-    fun dispatcherServletRegistrationBean(): DispatcherServletRegistrationBean {
-        // Create servlet context for page handling
-        val servletContext = AnnotationConfigWebApplicationContext()
-        servletContext.scan("com.meeting.demo.controller")
-        
-        // Create dispatcher servlet with context
-        val dispatcherServlet = DispatcherServlet(servletContext)
-        
-        // Register with specific path mapping for pages
-        val registration = DispatcherServletRegistrationBean(dispatcherServlet, "/page/*")
-        registration.setName("pageDispatcher")
-        registration.setLoadOnStartup(2) // Load after main dispatcher (which is 1)
-        
-        return registration
-    }
-}
+class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
